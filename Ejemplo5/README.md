d36b2b8d49fc88cdf9bb17da27d0e0d46431d15f
aquiles2001 <aquiles.lencina@mi.unc.edu.ar>
2024-02-17 17:28:45 -0300

    V1.0

---
+++ Ejemplo5/README.md
@@ -0,0 +1,13 @@
+# Ejemplo 5: ContDownLatch
+
+Se tendrá un hilo que representa la conferencia propiamente dicha asociado a un objeto de una
+clase llamada Videoconference y diez hilos que representarán diez participantes (asociados a objetos
+de la clase Participant). En el objeto de Videoconference (llamado conference) se inicializará me-
+diante el constructor un CountDownLatch llamado controller con el valor diez, indicando que deben
+realizarse diez operaciones para que su contador llegue a cero. El código del método run() de con-
+ference no se ejecutará hasta que se realicen las diez operaciones ya que contiene la instrucción
+controller.await();
+Los participantes por su parte ejecutarán conference.arrive() luego de haber dormido durante un
+tiempo aleatorio. En el método arrive() de la clase Videoconference se ejecuta controller.countDown()
+de manera que los hilos asociados a los participantes irán decrementando el contador del Count-
+DownLatch y cuando este sea cero el hilo asociado a la conferencia podrá seguir con su ejecución.
---
+++ Ejemplo5/src/Videoconference.java
@@ -0,0 +1,46 @@
+package Ejemplo5.src;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Videoconference implements Runnable{
+
+    /*
+     * This class uses a CountDownLatch to control the arrival of all the
+     */
+    private final CountDownLatch controller;
+
+    /*
+     * Constructor of the class. Initializes the CountDownLatch
+     * @param number The number of participants in the videoconference
+     */
+    public Videoconference(int number) {
+        controller = new CountDownLatch(number);
+    }
+
+    /*
+     * This method is called by each participant when he incorporates to the
+     * VideoConference
+     * @param name The name of the participant that incorporates to the
+     */
+    public void arrive(String name) {
+        System.out.printf("%s has arrived.", name);
+        controller.countDown();
+        System.out.printf("Videoconference: Waiting for %d participants.\n", controller.getCount());
+    }
+
+    /*
+     * Core method of the videoconference. It waits for all the participants
+     * to arrive
+     */
+    @Override
+    public void run() {
+        System.out.printf("Videoconference: Initialization: %d participants.\n", controller.getCount());
+        try {
+            controller.await();
+            System.out.printf("Videoconference: All the participants have come\n");
+            System.out.printf("Videoconference: Let's start...\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
